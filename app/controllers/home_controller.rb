a709cf684e737b3ddb0f8bfa57d167731b9a701c
rennyallen <rennyallen@hotmail.com>
2017-05-23 15:10:46 +0800

    analyse text

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,25 @@
 class HomeController < ApplicationController
   def index
+    @bullet = []
+  end
+
+  def create
+    separate
+    render :index
+  end
+
+  def separate
+    first_line = params[:doc].lines.first
+    @header = (first_line.include?("------")) ? params[:doc].lines.second.chomp : first_line
+    params[:doc].each_line do |line|
+      @quotation = $1 if line.match(/(".*")/)
+      @bullet.push $1.chomp if line.match(/(.*:.*)/m)
+    end
+  end
+
+  private
+
+  def analysis
+
   end
 end
