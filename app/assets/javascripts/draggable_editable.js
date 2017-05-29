495d7b8dc655b6f87c7bda8ac48a9440386e34bf
rennyallen <rennyallen@hotmail.com>
2017-05-29 22:55:03 +0800

    text options

--- app/assets/javascripts/draggable_editable.js
+++ app/assets/javascripts/draggable_editable.js
@@ -2,14 +2,14 @@ $(function() {
   $(".draggable")
     .draggable()
     .click(function(){
-        if ( $(this).is('.ui-draggable-dragging') ) {
-            return;
-        }
-        $(this).draggable("option", "disabled", true );
-        $(this).attr('contenteditable', true);
+      if ( $(this).is('.ui-draggable-dragging') ) {
+        return;
+      }
+      $(this).draggable("option", "disabled", true );
+      $(this).attr('contenteditable', true);
     })
     .blur(function(){
-        $(this).draggable('option', 'disabled', false);
-        $(this).attr('contenteditable', false);
+      $(this).draggable('option', 'disabled', false);
+      $(this).attr('contenteditable', false);
     })
 });
---
+++ app/assets/javascripts/result.js
@@ -0,0 +1,26 @@
+$(function(){
+  toolbar = document.getElementById('toolbar');
+}); 
+
+function select_element(obj){
+  toolbar.style.display = "block";
+  obj.id = 'item-selected';
+
+  $("#item-selected").blur(function(){
+    $(this).removeAttr('id');
+    toolbar.style.display = "none";
+  });
+}
+
+function delete_element(){
+  document.getElementById('item-selected').id = 'item-deleted';
+  refresh_trash(); 
+}
+
+function refresh_trash(){
+  var trash = document.getElementById('trash');
+  var deleted = document.getElementById('item-deleted');
+  deleted.classList.add('item-deleted');
+  trash.appendChild(deleted.cloneNode(true));   // add to trash
+  deleted.parentNode.removeChild(deleted);  //remove original one
+}
