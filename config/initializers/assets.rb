012b31a00f691f9773a2af5524e996a609d410f3
rennyallen <rennyallen@hotmail.com>
2017-07-06 01:28:12 +0800

    fix assets

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -8,4 +8,4 @@ Rails.application.config.assets.version = '1.0'
 
 # Precompile additional assets.
 # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
-# Rails.application.config.assets.precompile += %w( bootstrap.min.css font-awesome.min.css magnific-popup.css main.css )
+Rails.application.config.assets.precompile += %w( bootstrap.min.css font-awesome.min.css magnific-popup.css main.css )
