9c4a523a8cf41369e8234d1b9bb474b52ef88639
rennyallen <rennyallen@hotmail.com>
2017-07-06 23:15:04 +0800

    change route

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,10 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   devise_for :users
-  root 'home#index', as: 'root'
+  root 'home#landing', as: 'root'
   resources :home, only: [:index, :create]
 
+  get 'home', to: 'home#index'
   get 'landing_page', to: 'home#landing'
   get 'download', to: 'home#download'
   get 'home/refresh_part'
